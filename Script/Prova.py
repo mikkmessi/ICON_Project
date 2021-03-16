1b8a89575f33aa072f51d4b863143372c1bea846
Michele Messina <m.messina11@studenti.uniba.it>
2021-03-16 19:35:13 +0100

    Update Dataset_g26
    Aggiornato dataset, dovrebbe essere il definitivo

--- Script/Prova.py
+++ Script/Prova.py
@@ -31,16 +31,16 @@ from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 
-stats=pd.read_excel("C:\\Users\kecco\\Desktop\\Dataset\\TuttoInsiemeOrdinato.xlsx")
+stats=pd.read_excel("D:\\UniDispense\\ICON\\ICON_Project\\Dataset\\TuttoInsiemeOrdinato.xlsx")
 
 modeling=stats.copy()
 modeling=modeling.drop(['Giocatori_ID','TuttoInsiemeOrdinato_Nome_Cognome','Ruolo','Squadra'], axis=1)
 
-corr=modeling.corr()
-mask=np.zeros_like(corr, dtype=np.bool)
-mask[np.triu_indices_from(mask)]=True
-f,ax=plt.subplots(figsize=(11,9))
-cmap=sns.diverging_palette(0,150,as_cmap=True)
-sns.heatmap(corr,mask=mask,cmap=cmap,center=0,square=True,linewidths=.5,cbar_kws={"shrink":.5})
+corr = modeling.corr()
+mask = np.zeros_like(corr, dtype=bool)
+mask[np.triu_indices_from(mask)] = True
+f, ax = plt.subplots(figsize=(22, 18))
+cmap = sns.diverging_palette(0, 150, as_cmap=True)
+sns.heatmap(corr, mask=mask, cmap=cmap, center=0, square=True, linewidths=.5, cbar_kws={"shrink": .5})
 plt.show()
 
