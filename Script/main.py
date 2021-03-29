bfe392c53aff8a0cb95926406fcd367a618ff6a7
Francesco <kecco.zinga@gmail.com>
2021-03-29 20:31:07 +0200

    Added Best XI

--- Script/main.py
+++ Script/main.py
@@ -5,9 +5,9 @@ Created on 10 mar 2021
 '''
 import performance as p
 
-# file_path_1 = "https://github.com/mikkmessi/ICON_Project/blob/main/Dataset/Dataset_g26_NOPOR.xlsx"
+#file_path_1 = "C:\\Users\kecco\\Documents\\GitHub\ICON_Project\\Dataset\\Dataset_g26_NOPOR.xlsx"
 
-# file_path_2 = "https://github.com/mikkmessi/ICON_Project/blob/main/Dataset/Dataset_g27_NOPOR.xlsx"
+#file_path_2 = "C:\\Users\kecco\\Documents\\GitHub\ICON_Project\\Dataset\\Dataset_g27_NOPOR.xlsx"
 
 file_path_1 = "D:\\UniDispense\\ICON\\ICON_Project\\Dataset\\Dataset_g26_NOPOR.xlsx"
 
@@ -16,7 +16,7 @@ file_path_2 = "D:\\UniDispense\\ICON\\ICON_Project\\Dataset\\Dataset_g27_NOPOR.x
 dataset = p.load_and_model(file_path_1)
 
 X_train_std, X_test_std, Y_train, Y_test = p.split(dataset)
-
+ 
 dict_models, best_model = p.batch_classify(X_train_std, Y_train, X_test_std, Y_test)
 
 my_team, copia = p.team(file_path_2)
